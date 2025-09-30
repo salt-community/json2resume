c484be3e4e09a735ae4ec29e21f2fc766fe78b3a
David Aslan <davidaslan112671@live.se>
2025-09-30 15:12:27 +0200

    added templateEngine tests

--- frontend/src/routes/pdf.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/pdf')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/pdf"!</div>
-}
